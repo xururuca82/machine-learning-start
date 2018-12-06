b00f9700b832b0170b498838c034bece6b2382f6
1024dream-jhyoun <1024dream.jhyoun@gmail.com>
2018-12-06 11:36:31 +0900

    chapter4-7 데이터를 검증하는 방법

---
+++ 04-머신러닝/07-데이터를 검증하는 방법/03-grid-mnist.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn import model_selection, svm, metrics
+from sklearn.model_selection import GridSearchCV
+
+# MNIST 학습 데이터 읽어 들이기
+train_csv = pd.read_csv("./mnist/train.csv")
+test_csv = pd.read_csv("./mnist/t10k.csv")
+
+# 필요한 열 추출하기
+train_label = train_csv.iloc[:, 0]
+train_data = train_csv.iloc[:, 1:577]
+test_label = test_csv.iloc[:, 0]
+test_data = test_csv.iloc[:, 1:577]
+print("학습 데이터의 수 = ", len(train_label))
+
+# 그리드 서치 매개변수 설정
+params = [
+    {"C": [1, 10, 100, 1000], "kernel": ["linear"]},
+    {"C": [1, 10, 100, 1000], "kernel": ["rbf"], "gamma":[0.001, 0.0001]}
+]
+
+# 그리드 서치 수행
+clf = GridSearchCV(svm.SVC(), params, n_jobs=-1)
+clf.fit(train_data, train_label)
+print("학습기= ", clf.best_estimator_)
+
+# 테스트 데이터 확인하기
+pre = clf.predict(test_data)
+ac_scoer = metrics.accuracy_score(pre, test_label)
+print("정답률 =", ac_scoer)
+
+
+
